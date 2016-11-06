22239fdcb5a2fc5cb5e58464cbb27e236e11f55f
Gio d'Amelio <giodamelio@gmail.com>
2016-11-05 18:36:59 -0700

    Basic library struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,25 @@
+pub struct Edo {
+
+}
+
+impl Edo {
+    pub fn new() -> Edo {
+        Edo {
+        }
+    }
+
+    fn parse(self) {
+        println!("Hello World!");
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::Edo;
+
     #[test]
-    fn it_works() {
+    fn parse_template() {
+        let parser = Edo::new();
+        parser.parse();
     }
 }
