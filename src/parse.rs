129372c66ad600a9e9a9e5d46984f0dd852cb6c1
Gio d'Amelio <giodamelio@gmail.com>
2016-11-20 22:11:26 -0800

    Add TODO to argument parsing

--- src/parse.rs
+++ src/parse.rs
@@ -14,6 +14,7 @@ pub enum Expression<'a> {
 }
 
 // Parse a list of arguments
+// TODO: allow non alphanumeric values inside arguments
 // TODO: allow trailing commas, allow leading and trailing whitespace
 named!(arguments<&[u8], Vec<&str> >, delimited!(
     char!('('),
